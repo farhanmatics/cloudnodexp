8d00fc065ce4ae11a7f24fc8938e7bed95b938e2
Farhan Asif <farhanmatics@gmail.com>
2024-01-13 22:09:58 +0000

    init

---
+++ app.js
@@ -0,0 +1,69 @@
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const morgan = require('morgan')
+const path = require('path')
+const cookieParser = require('cookie-parser')
+const cors = require('cors')
+
+
+
+
+
+app.use(morgan('dev'));
+app.use(cookieParser())
+
+app.use('/uploads', express.static('uploads'))
+app.use('/js', express.static('public/js'))
+app.use('/public', express.static('public'))
+
+
+// Set Templating Engine
+
+app.set('view engine', 'ejs')
+
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+
+app.use(cors())
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
+    res.header('Access-Control-Allow-Credentials', true); // If needed
+
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET')
+        return res.status(200).json({})
+    }
+    next();
+})
+
+
+app.get('/', (req, res) => {
+  
+  res.render('index')
+})
+
+app.use((req, res, next) => {
+    const error = new Error('Not found')
+    error.status = 404
+    next(error)
+});
+
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500)
+    res.json({
+        message: error.message
+    })
+})
+
+module.exports = app
+
